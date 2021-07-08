df1b93ef38420bd1bf2579f4861bade1b9d32b06
Bit <bit@bitknows.tech>
2021-07-08 06:11:07 -0700

    added search function, removed cards file

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 import re
+import asyncio
+import re
+
+import aiohttp
+import discord
+import urllib.parse as urlparse
 
 import discord
-
-from cards import *
 
 client = discord.Client()
 
@@ -29,5 +33,76 @@ async def on_message(message: discord.Message):
                         cards = await getCardByName(group)
                         for card in cards:
                             await message.channel.send(embed=card)
+
+COLOUR = discord.Colour(0x8b23b8)
+
+async def getRequest(url, **kwargs):
+    await asyncio.sleep(0.1)
+    async with aiohttp.ClientSession(loop=client.loop) as session:
+        response = await session.get(url, **kwargs)
+        return await response.json()
+
+async def getCardByName(name: str) -> list[discord.Embed]:
+    card = await getRequest(url='http://api.scryfall.com/cards/named?', params={'fuzzy':name})
+    messages: list[discord.Embed] = list()
+
+    if card['object'] == 'error':
+        return [discord.Embed(
+            description = "an error has occured. {}".format(re.sub(r'\(|\'|,|\)+', '', card['details']))
+        ),]
+    
+    if 'card_faces' in card:
+        for entry in card['card_faces']:
+            message = discord.Embed(
+                title="**{}**".format(entry['name']),
+                url=card['scryfall_uri'],
+                color=COLOUR
+            )
+            message.set_image(url=entry['image_uris']['normal'])
+            messages.append(message)
+        return messages
+    
+    message = discord.Embed(
+        title="**{}**".format(card['name']),
+        url=card['scryfall_uri'],
+        color=COLOUR
+    )
+
+    message.set_image(url=card['image_uris']['normal'])
+    messages.append(message)
+    return messages
+
+async def getCardFromSearch(search: str) -> list[discord.Embed]:
+    cards = await getRequest(url='http://api.scryfall.com/cards/search?', params={'q':search})
+    messages: list[discord.Embed]= list()
+    
+    # returns if error ie. no results found
+    if cards['object'] == 'error':
+        return [discord.Embed(
+            description = "an error has occured. {}".format(re.sub(r'\(|\'|,|\)+', '', cards['details']))
+        ),]
+
+    # returns the first result of the search
+    firstResult = cards['data'][0]
+    if 'card_faces' in firstResult:
+        for entry in firstResult['card_faces']:
+            message = discord.Embed(
+                title="**{}**".format(entry['name']),
+                url=firstResult['scryfall_uri'],
+                color=COLOUR
+            )
+            message.set_image(url=entry['image_uris']['normal'])
+            messages.append(message)
+        return messages
+    
+    message = discord.Embed(
+        title="**{}**".format(firstResult['name']),
+        url=firstResult['scryfall_uri'],
+        color=COLOUR
+    )
+
+    message.set_image(url=firstResult['image_uris']['normal'])
+    messages.append(message)
+    return messages
 
 client.run(os.environ.get("DISCORD"))
